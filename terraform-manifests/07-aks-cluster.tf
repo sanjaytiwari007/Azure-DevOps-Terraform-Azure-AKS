fcb5f450774eb31b444d4bd38aa2f98df89fb2fd
Sanjay Tiwari <65133206+sanjaytiwari007@users.noreply.github.com>
2021-06-10 18:26:26 -0400

    Update files for warnings

--- terraform-manifests/07-aks-cluster.tf
+++ terraform-manifests/07-aks-cluster.tf
@@ -1,6 +1,6 @@
 resource "azurerm_kubernetes_cluster" "aks_cluster" {
-  dns_prefix          = "${azurerm_resource_group.aks_rg.name}"
-  location            = azurerm_resource_group.aks_rg.location
+  dns_prefix          =  azurerm_resource_group.aks_rg.name
+  location            =  azurerm_resource_group.aks_rg.location
   name                = "${azurerm_resource_group.aks_rg.name}-cluster"
   resource_group_name = azurerm_resource_group.aks_rg.name
   kubernetes_version  = data.azurerm_kubernetes_service_versions.current.latest_version
